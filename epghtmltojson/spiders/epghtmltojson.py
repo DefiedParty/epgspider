33a8b81ca7f89c828c8378fd5a443bca5d91212a
DefiedParty <dp@dpii.club>
2021-01-18 23:00:55 +0800

    参数传入

--- epghtmltojson/spiders/epghtmltojson.py
+++ epghtmltojson/spiders/epghtmltojson.py
@@ -12,11 +12,10 @@ programInfoList = {"start": 0, "stop": 0, "title": ""}
 class epgSpider(scrapy.Spider):
     name = "epgjson"
 
+    def __init__(self, chNameCN=None,targetDate=None, *args, **kwargs):
+        super(epgSpider, self).__init__(*args, **kwargs)
+
     def start_requests(self):
-        global chNameCN
-        global targetDate
-        chNameCN = getattr(self, chNameCN, False)
-        targetDate = getattr(self, targetDate, False)
         urls = [
             'http://epg.51zmt.top:8000/api/i/?ch=%s&date=%s'%(chNameCN,targetDate),
         ]
