7e7fe8781d6fa4a455e253883b0a16d21c2cf70e
DefiedParty <dp@dpii.club>
2021-01-18 22:34:02 +0800

    完善json保存

--- epghtmltojson/spiders/epghtmltojson.py
+++ epghtmltojson/spiders/epghtmltojson.py
@@ -1,29 +1,36 @@
 import scrapy
 import json
+import copy
 
 chName = "fhzw"
-jsonList = {"fhzw":[]}
-programInfoList = {"start":0,"stop":0,"title":""}
+chNameCN = "凤凰中文"
+targetDate="2021-01-15"
+jsonList = {chName: []} 
+programInfoList = {"start": 0, "stop": 0, "title": ""}
+
 
 class epgSpider(scrapy.Spider):
     name = "epgjson"
 
     def start_requests(self):
+        global chNameCN
+        global targetDate
+        chNameCN = getattr(self, chNameCN, False)
+        targetDate = getattr(self, targetDate, False)
         urls = [
-            'http://epg.51zmt.top:8000/api/i/?ch=%E5%87%A4%E5%87%B0%E4%B8%AD%E6%96%87&date=2021-01-15',
+            'http://epg.51zmt.top:8000/api/i/?ch=%s&date=%s'%(chNameCN,targetDate),
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        list =response.css('li')
+        list = response.css('li')
+        print(chNameCN)
         for program in list:
-            print("111111")            
-            programInfoList["start"]=program.css('td::text')[0].extract()
-            programInfoList["title"]=program.css('td::text')[1].extract()
-            jsonList["fhzw"].append(programInfoList)
-            print("22")
-        output=json.dumps(jsonList)
+            programInfoList["start"] = program.css('td::text')[0].extract()
+            programInfoList["title"] = program.css('td::text')[1].extract()
+            jsonList["fhzw"].append(copy.copy(programInfoList))
+        output = json.dumps(jsonList)
         #page = response.url.split("/")[-2]
         filename = f'fullepg.json'
         with open(filename, 'w') as f:
