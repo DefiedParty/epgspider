fada821af0892ef0afd326041359c09363c3e7f9
DefiedParty <dp@dpii.club>
2021-05-02 02:45:22 +0800

    Add:freedom date set function;merge script buffer.Fix:code format;"if" judge logic.
    Add:freedom date set function;merge script buffer.
    Fix:code format;"if" judge logic.

--- epg_get.sh
+++ epg_get.sh
@@ -1,39 +1,31 @@
 #/bin/bash
 
 num=1
-ababa=1
 count=$1
-for data in `cat channel_list.cfg`
-do
-    if [ $(($num%2)) != 0 ]
-    then 
-    chNameCN[${num}]=${data}
+for data in $(cat channel_list.cfg); do
+    if [ $(($num % 2)) != 0 ]; then
+        chNameCN[${num}]=${data}
     else
-    chname[${num-1}]=${data}
-    while (( $count<$2 ))
-    do
-        thedate=`date  +"%Y-%m-%d" -d  "${count} days"`
-        if [ $3=="m" ]
-        then
-            if [ ! -f "epg_db/${chNameCN[num-1]} ${thedate}.epg.${chname[num]}" ]
-            then
-                scrapy crawl epgjson -a chname="${chname[num]}" -a chNameCN="${chNameCN[num-1]}" -a targetDate="${thedate}" -a fileName="${chNameCN[num-1]} ${thedate}.epg.${chname[num]}" &> /dev/null
-                if [ ! -d "epg_db/" ]
-                then
-                    mkdir epg_db
+        chname[${num-1}]=${data}
+        while [ $count -le $2 ]; do
+            thedate=$(date +"%Y-%m-%d" -d "${count} days")
+            if [ "$3" == "m" ]; then
+                if [ ! -f "epg_db/${chNameCN[num - 1]} ${thedate}.epg.${chname[num]}" ]; then
+                    scrapy crawl epgjson -a chname="${chname[num]}" -a chNameCN="${chNameCN[num - 1]}" -a targetDate="${thedate}" -a fileName="${chNameCN[num - 1]} ${thedate}.epg.${chname[num]}" &>/dev/null
+                    if [ ! -d "epg_db/" ]; then
+                        mkdir epg_db
+                    fi
+                    mv *.epg.* epg_db/
                 fi
-                mv *.epg.* epg_db/
+            else
+                scrapy crawl epgjson -a chname="${chname[num]}" -a chNameCN="${chNameCN[num - 1]}" -a targetDate="${thedate}" -a fileName="${chNameCN[num - 1]} ${thedate}.json" &>/dev/null
             fi
-        else
-            scrapy crawl epgjson -a chname="${chname[num]}" -a chNameCN="${chNameCN[num-1]}" -a targetDate="${thedate}" -a fileName="${chNameCN[num-1]} ${thedate}.json" &> /dev/null
-        fi
-        let count++
-    done
+            let count++
+        done
     fi
     let num++
     count=$1
 done
-if [ $3=="m" ]
-then
- bash ./mergeAllEPG.sh &> /dev/null
+if [ "$3" == "m" ]; then
+    bash ./mergeAllEPG.sh $1 $2 #&>/dev/null
 fi
--- mergeAllEPG.sh
+++ mergeAllEPG.sh
@@ -4,18 +4,34 @@ fi
 if [ ! -d "json_files/" ]; then
     mkdir json_files
 fi
-if [ ! -d epg_db/*.epg.* ]; then
+if [[ ! -n epg_db/*.epg.* ]]; then
     echo 没有找到节目单~
     exit
 fi
-cp epg_db/*.epg.* epg_files/
+num=1
+count=$1
+for data in $(cat channel_list.cfg); do
+    if [ $(($num % 2)) != 0 ]; then
+        chNameCN[${num}]=${data}
+    else
+        chname[${num-1}]=${data}
+        while [ $count -le $2 ]; do
+            thedate=$(date +"%Y-%m-%d" -d "${count} days")
+            cp "epg_db/${chNameCN[num - 1]} ${thedate}.epg.${chname[num]}" epg_files/
+            let count++
+        done
+    fi
+    let num++
+    count=$1
+done
+#cp epg_db/*.epg.* epg_files/
 if [ ! -d fullepgs.json ]; then
     touch fullepgs.json
 fi
-for keys in `jq -r 'keys|.[]' epg_files/*.epg.*`
-do
-    jq  -a ".${keys}|.[]" epg_files/*.${keys} | jq -a -s 'reduce . as $item ([]; . +$item)' | jq -a "{"${keys}":.}" &> json_files/${keys}.json
+for keys in $(jq -r 'keys|.[]' epg_files/*.epg.*); do
+    jq -a ".${keys}|.[]" epg_files/*.${keys} | jq -a -s 'reduce . as $item ([]; . +$item)' | jq -a "{"${keys}":.}" &>json_files/${keys}.json
 done
 rm -rf epg_files/*
-jq -a -s 'reduce .[] as $item ({}; .+$item)' json_files/*.json &> fullepgs.json
+jq -a -s 'reduce .[] as $item ({}; .+$item)' json_files/*.json &>fullepgs.json
 rm -rf json_files/*
+echo "本次合并了从 $(date +"%Y-%m-%d" -d "$1 days") 至 $(date +"%Y-%m-%d" -d "$2 days") 总共 $(((${num} - 1) / 2)) 个频道的节目单，已经存到项目根目录下的 fullepgs.json "
